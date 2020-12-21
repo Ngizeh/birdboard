be71ec305e27f5e7d001a9d1288644e71c480083
Ngizeh Mwas <edngize@gmail.com>
2020-12-21 21:53:59 +0300

    Project belongs to user relationship

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Project;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,5 +19,14 @@ class ProjectTest extends TestCase
 
     	$this->assertEquals('/projects/'.$project->id, $project->path());
     }
+
+    /** @test **/
+    public function belongs_to_the_users()
+    {
+    	$project = factory(Project::class)->create();
+
+    	$this->assertInstanceOf(User::class, $project->owner);
+    }
+
 
 }
