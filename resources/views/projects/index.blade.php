222db55367de67f699744404890a3352ce041cb4
Ngizeh Mwas <edngize@gmail.com>
2020-12-23 17:01:32 +0300

    Layout UI is done

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,16 @@
  @extends('layouts.app')
 
  @section('content')
-  <a href="{{ route('projects.create') }}">Create Project</a>
-	<ul>
-		@forelse ($projects as $project)
-		<li>
-			<a href="{{ route('projects.show', $project->id) }}">{{ $project->title }}</a>
-		</li>
-		@empty
-		 <p>You don't have projects yet</p>
-		@endforelse
-	</ul>
-
+ <div class="flex justify-between items-end mb-3 py-6 ">
+ 	<h3 class="text-gray-500">My Projects</h3>
+ 	<a href="{{ route('projects.create') }}" class="bg-blue-400 py-3 px-6 rounded-lg text-white">Add Project</a>
+ </div>
+ <div class="grid grid-cols-1 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 gap-4">
+ 	@forelse ($projects as $project)
+ 	  @include('partials.card')
+ 	@empty
+ 	<div>You don't have projects yet</div>
+ 	@endforelse
+ </div>
 
  @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,8 +2,28 @@
 
  @section('content')
 
- <h3>{{ $project->title }}</h3>
- <div>{{ $project->description }}</div>
- <a href="{{ route('projects.index') }}">Back</a>
+ <header>
+ 	<div class="flex items-center py-4">
+ 		<h3 class="text-gray-400 font-normal pr-4">My Projects / {{ $project->title}} </h3>
+ 		<a href="#" class="bg-blue-400 py-2 px-6 text-white rounded-lg">Add Task</a>
+ 	</div>
+ </header>
 
+ <main>
+ 	<div class="flex">
+ 		<div class="w-3/4 mr-8">
+ 			<div class="card mb-3">{{ $project->title }}</div>
+ 			<div class="card mb-3">{{ $project->title }}</div>
+ 			<div class="card mb-3">{{ $project->title }}</div>
+ 			<div class="card mb-3">{{ $project->title }}</div>
+ 			<div class="card mb-3">{{ $project->title }}</div>
+
+ 			<h3 class="text-gray-400 font-normal pr-4 mt-6 mb-3">General Notes </h3>
+ 			<textarea class="card mb-3 w-full h-32"></textarea>
+ 		</div>
+ 		<div class="w-1/4">
+ 			@include('partials.card')
+ 		</div>
+ 	</div>
+ </main>
  @endsection
